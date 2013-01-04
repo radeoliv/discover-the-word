acb7d3ad69cb282ee99844003a6323ede5d11798
bhmyamashita@gmail.com <bhmyamashita@gmail.com@16804349-9818-1494-bbcb-5f11557c2100>
2013-01-04 05:43:08 +0000

    Inserido o sistema de score / Bug encontrado: Botao de voltar buga tudo.

--- trunk/ discover-the-word/GameStartScreen/src/com/example/trainingandroid/GameScreen.java
+++ trunk/ discover-the-word/GameStartScreen/src/com/example/trainingandroid/GameScreen.java
@@ -8,6 +8,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -176,9 +177,4 @@ public class GameScreen extends Activity {
 		startActivity(endGameIntent);
 	}
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
 }
